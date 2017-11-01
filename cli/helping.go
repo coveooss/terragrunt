a1c116d40912acd4350f61a1567de782bfdd7c80
Jocelyn Giroux <jgiroux@coveo.com>
2017-11-01 15:36:02 -0400

    Allow get-doc to only list elements

--- cli/helping.go
+++ cli/helping.go
@@ -2,12 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 	"sort"
 	"strings"
 
 	"github.com/fatih/color"
+	logging "github.com/op/go-logging"
 
 	"github.com/gruntwork-io/terragrunt/config"
 	"github.com/gruntwork-io/terragrunt/options"
@@ -22,12 +24,15 @@ var item = color.New(color.FgHiYellow).SprintFunc()
 func PrintVersions(terragruntOptions *options.TerragruntOptions, conf *config.TerragruntConfig) {
 	fmt.Println("Terragrunt version", terragruntVersion)
 	fmt.Println("Terraform version", terraformVersion)
+
 	for _, extraCmd := range conf.ExtraCommands {
 		if extraCmd.VersionArg == "" || len(extraCmd.OS) > 0 && !util.ListContainsElement(extraCmd.OS, runtime.GOOS) {
 			continue
 		}
 
-		fmt.Printf("\n%s\n", item(extraCmd.Name))
+		if strings.Contains(extraCmd.Name, " ") {
+			fmt.Printf("\n%s\n", item(extraCmd.Name))
+		}
 		if extraCmd.Commands == nil {
 			if extraCmd.Command == "" {
 				extraCmd.Commands = []string{extraCmd.Name}
@@ -36,10 +41,22 @@ func PrintVersions(terragruntOptions *options.TerragruntOptions, conf *config.Te
 			}
 		}
 		for _, cmd := range extraCmd.Commands {
-			title.Printf("\n%s ", cmd)
-
-			if err := shell.RunShellCommand(terragruntOptions, cmd, extraCmd.VersionArg); err != nil {
-				terragruntOptions.Logger.Error(err)
+			loggingLevel := logging.GetLevel("")
+			if loggingLevel == logging.NOTICE {
+				logging.SetLevel(logging.WARNING, "")
+			}
+			os.Setenv("TERRAGRUNT_COMMAND", cmd)
+			args := []string{extraCmd.VersionArg}
+			if strings.ContainsAny(extraCmd.VersionArg, " |,&$") {
+				cmd = "bash"
+				args = util.ExpandArguments([]string{"-c", extraCmd.VersionArg}, terragruntOptions.WorkingDir)
+			}
+			out, err := shell.RunShellCommandAndCaptureOutput(terragruntOptions, false, cmd, args...)
+			logging.SetLevel(loggingLevel, "")
+			if err != nil {
+				terragruntOptions.Logger.Infof("Got %s %s while getting version for %s", color.RedString(err.Error()), out, cmd)
+			} else {
+				fmt.Println(strings.TrimSpace(out))
 			}
 		}
 	}
@@ -47,28 +64,45 @@ func PrintVersions(terragruntOptions *options.TerragruntOptions, conf *config.Te
 
 // PrintDoc prints the contextual documentation relative to the current project
 func PrintDoc(terragruntOptions *options.TerragruntOptions, conf *config.TerragruntConfig) {
-	fmt.Println(conf.Description)
-	title.Println("Extra arguments: (in evaluation order):")
-	fmt.Println(util.Indent(extraArgs(conf.Terraform.ExtraArgs), 4))
+	doc := printDoc{
+		util.ListContainsElement(terragruntOptions.TerraformCliArgs[1:], "list"),
+	}
 
-	title.Println("File importers (in execution order):")
-	fmt.Println(util.Indent(importers(conf.ImportFiles, terragruntOptions.WorkingDir), 4))
+	printTitle := title.Print
+	if !doc.listOnly {
+		printTitle = title.Println
+		fmt.Println(conf.Description)
+	}
 
-	title.Println("Pre hooks (in execution order):")
-	fmt.Println(util.Indent(hooks(conf.PreHooks, false), 4))
-	fmt.Println(util.Indent(item("Initialize Terraform state\n"), 4))
-	fmt.Println(util.Indent(hooks(conf.PreHooks, true), 4))
+	printTitle("Extra arguments: (in evaluation order):")
+	fmt.Println(util.Indent(doc.extraArgs(conf.Terraform.ExtraArgs), 4))
 
-	title.Println("Post hooks (in execution order):")
-	fmt.Println(util.Indent(hooks(conf.PostHooks), 4))
+	printTitle("File importers (in execution order):")
+	fmt.Println(util.Indent(doc.importers(conf.ImportFiles, terragruntOptions.WorkingDir), 4))
 
-	title.Println("Extra commands available:")
-	fmt.Println(util.Indent(extraCommands(conf.ExtraCommands), 4))
+	printTitle("Pre hooks (in execution order):")
+	fmt.Println(util.Indent(doc.hooks(conf.PreHooks, false), 4))
+	printTitle("Initialize Terraform state")
+	fmt.Println(util.Indent(doc.hooks(conf.PreHooks, true), 4))
+
+	printTitle("Post hooks (in execution order):")
+	fmt.Println(util.Indent(doc.hooks(conf.PostHooks), 4))
+
+	printTitle("Extra commands available:")
+	fmt.Println(util.Indent(doc.extraCommands(conf.ExtraCommands), 4))
 }
 
-func extraArgs(extraArgs []config.TerraformExtraArguments) (out string) {
+type printDoc struct {
+	listOnly bool
+}
+
+func (pd *printDoc) extraArgs(extraArgs []config.TerraformExtraArguments) (out string) {
 	for _, args := range extraArgs {
-		out += fmt.Sprintf("\n%s\n", item(args.Name))
+		out += fmt.Sprintf("\n%s", item(args.Name))
+		if pd.listOnly {
+			continue
+		}
+		out += fmt.Sprintln()
 		if args.Description != "" {
 			out += fmt.Sprintf("\n%s\n", args.Description)
 		}
@@ -82,9 +116,13 @@ func extraArgs(extraArgs []config.TerraformExtraArguments) (out string) {
 	return
 }
 
-func importers(importers []config.ImportConfig, cwd string) (out string) {
+func (pd *printDoc) importers(importers []config.ImportConfig, cwd string) (out string) {
 	for _, importer := range importers {
-		out += fmt.Sprintf("\n%s\n", item(importer.Name))
+		out += fmt.Sprintf("\n%s", item(importer.Name))
+		if pd.listOnly {
+			continue
+		}
+		out += fmt.Sprintln()
 		if importer.Description != "" {
 			out += fmt.Sprintf("\n%s\n", importer.Description)
 		}
@@ -125,14 +163,18 @@ func importers(importers []config.ImportConfig, cwd string) (out string) {
 	return
 }
 
-func hooks(hooks []config.Hook, afterInitState ...bool) (out string) {
+func (pd *printDoc) hooks(hooks []config.Hook, afterInitState ...bool) (out string) {
 	sort.Sort(hooksByOrder(hooks))
 
 	for _, hook := range hooks {
 		if afterInitState != nil && hook.AfterInitState != afterInitState[0] {
 			continue
 		}
-		out += fmt.Sprintf("\n%s\n", item(hook.Name))
+		out += fmt.Sprintf("\n%s", item(hook.Name))
+		if pd.listOnly {
+			continue
+		}
+		out += fmt.Sprintln()
 		if hook.Description != "" {
 			out += fmt.Sprintf("\n%s\n", hook.Description)
 		}
@@ -161,16 +203,27 @@ func (e extraCommandsByName) Less(i, j int) bool {
 	return e[i].Name < e[j].Name
 }
 
-func extraCommands(extraCommands []config.ExtraCommand) (out string) {
+func (pd *printDoc) extraCommands(extraCommands []config.ExtraCommand) (out string) {
 	sort.Sort(extraCommandsByName(extraCommands))
 	for _, cmd := range extraCommands {
 		var aliases string
-		commands := append(cmd.Commands, cmd.Aliases...)
-		if commands != nil {
-			sort.Strings(commands)
-			aliases = fmt.Sprintf(" | %s", strings.Join(commands, " | "))
+		for _, alias := range cmd.Aliases {
+			alias = strings.Split(alias, "=")[0]
+			if alias == cmd.Name {
+				continue
+			}
+			cmd.Commands = append(cmd.Commands, alias)
 		}
-		out += fmt.Sprintf("\n%s%s\n", item(cmd.Name), aliases)
+		if cmd.Commands != nil {
+			cmd.Commands = util.RemoveDuplicatesFromListKeepFirst(cmd.Commands)
+			sort.Strings(cmd.Commands)
+			aliases = fmt.Sprintf(" | %s", strings.Join(cmd.Commands, " | "))
+		}
+		out += fmt.Sprintf("\n%s%s", item(cmd.Name), aliases)
+		if pd.listOnly {
+			continue
+		}
+		out += fmt.Sprintln()
 
 		if cmd.Description != "" {
 			out += fmt.Sprintf("\n%s\n", cmd.Description)
