f1d385abd5509ae1dd85b2f1cc5d165e6f6cc7b2
Jo Giroux <jgiroux@coveo.com>
2019-08-30 08:46:21 -0400

    Stop looking for files after a match
    Change globFiles to support two mode when many folders are provided, one to stop on match (act as a path) and the other to cumulate all found files in all paths.
    
    Remove some warnings in the code
    
    Refactor import_variables to add support for path search (many folders to support temporary output in a plan-all) and make it more object-oriented
    
    Support multiple nested_under values under import_variables to avoid requirement to load the same file multiple times.

--- config/extension_hook.go
+++ config/extension_hook.go
@@ -90,12 +90,12 @@ func (hook *Hook) run(args ...interface{}) (result []interface{}, err error) {
 	return
 }
 
-func (base Hook) setState(err error) {
+func (hook *Hook) setState(err error) {
 	exitCode, errCode := shell.GetExitCode(err)
 	if errCode != nil {
 		exitCode = -1
 	}
-	base.options().SetStatus(exitCode, err)
+	hook.options().SetStatus(exitCode, err)
 }
 
 // ----------------------- HookList -----------------------
