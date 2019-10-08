d63eb9cee5bff4d2d1d1de9102bf341b936ae85e
Jo Giroux <jgiroux@coveo.com>
2019-10-08 14:52:50 -0400

    Use real parallel tests

--- test/integration_variables_test.go
+++ test/integration_variables_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"sync"
 	"testing"
 
 	"github.com/gruntwork-io/terragrunt/util"
@@ -12,6 +11,7 @@ import (
 )
 
 func TestTerragruntImportVariables(t *testing.T) {
+	t.Parallel()
 	type test struct {
 		project        string
 		envVariables   map[string]string
@@ -90,32 +90,27 @@ func TestTerragruntImportVariables(t *testing.T) {
 			expectedOutput: "example = 1-2-1-2",
 		},
 	}
-	var wg sync.WaitGroup
-	for _, tt := range tests {
-		fmt.Println("hello", tt.project)
-		wg.Add(1)
-		go func(tt test) {
-			t.Run(tt.project, func(t *testing.T) {
-				defer wg.Done()
-				tmpEnvPath := copyEnvironment(t, tt.project)
-				defer os.RemoveAll(tmpEnvPath)
-				rootPath := util.JoinPath(tmpEnvPath, tt.project)
+	for _, test := range tests {
+		tt := test // tt must be unique see https://github.com/golang/go/issues/16586
+		t.Run(tt.project, func(t *testing.T) {
+			t.Parallel()
+			tmpEnvPath := copyEnvironment(t, tt.project)
+			defer os.RemoveAll(tmpEnvPath)
+			rootPath := util.JoinPath(tmpEnvPath, tt.project)
 
-				for key, value := range tt.envVariables {
-					os.Setenv(key, value)
-					defer os.Unsetenv(key)
-				}
+			for key, value := range tt.envVariables {
+				os.Setenv(key, value)
+				defer os.Unsetenv(key)
+			}
 
-				var (
-					stdout bytes.Buffer
-					stderr bytes.Buffer
-				)
+			var (
+				stdout bytes.Buffer
+				stderr bytes.Buffer
+			)
 
-				runTerragruntRedirectOutput(t, fmt.Sprintf("terragrunt apply --terragrunt-non-interactive --terragrunt-working-dir %s", rootPath), &stdout, &stderr)
-				output := stdout.String()
-				assert.Contains(t, output, tt.expectedOutput)
-			})
-		}(tt)
+			runTerragruntRedirectOutput(t, fmt.Sprintf("terragrunt apply --terragrunt-non-interactive --terragrunt-working-dir %s", rootPath), &stdout, &stderr)
+			output := stdout.String()
+			assert.Contains(t, output, tt.expectedOutput)
+		})
 	}
-	wg.Wait()
 }
