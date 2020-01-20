70b17ae19d08bd0951261310406b49f4a799aedc
Julien Duchesne <jduchesne@coveo.com>
2020-01-20 10:31:29 -0500

    Use HCL2 importing logic from Terraform's code
    This removes the need for all the custom logic we have to read Terraform files
    Since the Terraform typing system is really inflexible, we need to serialize values to JSON and deserialize them to get the value as an interface{}

--- go.mod
+++ go.mod
@@ -10,10 +10,9 @@ require (
 	github.com/coveooss/multilogger v0.4.4
 	github.com/fatih/color v1.9.0
 	github.com/go-errors/errors v1.0.1
-	github.com/google/go-cmp v0.3.1 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
-	github.com/hashicorp/go-getter v1.4.1
+	github.com/hashicorp/go-getter v1.4.2-0.20200106182914-9813cbd4eb02
 	github.com/hashicorp/go-version v1.2.0
+	github.com/hashicorp/terraform v0.12.19
 	github.com/matryer/try v0.0.0-20161228173917-9ac251b645a2 // indirect
 	github.com/mattn/go-zglob v0.0.1
 	github.com/mitchellh/mapstructure v1.1.2
@@ -22,10 +21,9 @@ require (
 	github.com/stretchr/testify v1.4.0
 	github.com/ulikunitz/xz v0.5.6 // indirect
 	github.com/urfave/cli v1.22.2
+	github.com/zclconf/go-cty v1.2.1
 	golang.org/x/sys v0.0.0-20191127021746-63cb32ae39b2 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/matryer/try.v1 v1.0.0-20150601225556-312d2599e12e
 	gopkg.in/yaml.v2 v2.2.7
 )
-
-replace github.com/coveooss/gotemplate/v3 => /home/jduchesne/repos/gotemplate
--- util/terraform.go
+++ util/terraform.go
@@ -3,57 +3,36 @@ package util
 import (
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
-	"sort"
 	"strings"
+
+	"github.com/hashicorp/terraform/configs"
 
 	"github.com/coveooss/gotemplate/v3/collections"
 	"github.com/coveooss/gotemplate/v3/hcl"
 	"github.com/coveooss/gotemplate/v3/json"
 	"github.com/coveooss/gotemplate/v3/template"
-	"github.com/coveooss/gotemplate/v3/utils"
 	"github.com/coveooss/gotemplate/v3/yaml"
 	"github.com/sirupsen/logrus"
+	ctyJson "github.com/zclconf/go-cty/cty/json"
 )
 
 // LoadDefaultValues returns a map of the variables defined in the tfvars file
-func LoadDefaultValues(folder string) (result map[string]interface{}, err error) {
-	result = map[string]interface{}{}
-	for _, file := range getTerraformFiles(folder) {
-		var fileVars map[string]interface{}
-		switch filepath.Ext(file) {
-		case ".tf":
-			fileVars, err = getDefaultVarsFromFile(file, hcl.Unmarshal)
-		case ".json":
-			fileVars, err = getDefaultVarsFromFile(file, json.Unmarshal)
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		for key, value := range fileVars {
-			if old, exist := result[key]; exist {
-				switch old := old.(type) {
-				case map[string]interface{}:
-					newValue, ok := value.(map[string]interface{})
-					if !ok {
-						return nil, fmt.Errorf("Cannot override %s: %T with %T", key, old, value)
-					}
-					value, err = utils.MergeDictionaries(old, newValue)
-					if err != nil {
-						return nil, err
-					}
-				}
-			}
-			result[key] = value
-		}
+func LoadDefaultValues(folder string) (map[string]interface{}, error) {
+	parser := configs.NewParser(nil)
+	terraformConfig, err := parser.LoadConfigDir(folder)
+	if err != nil {
+		return map[string]interface{}{}, fmt.Errorf("Caught error while trying to load default variable values: %v", err)
 	}
-
-	return
+	return getTerraformVariableValues(terraformConfig, false)
 }
 
 // LoadVariablesFromFile returns a map of the variables defined in the tfvars file
 func LoadVariablesFromFile(path, cwd string, applyTemplate bool, context ...interface{}) (map[string]interface{}, error) {
+	parser := configs.NewParser(nil)
+	if terraformConfig, err := parser.LoadConfigFile(path); err == nil {
+		return getTerraformVariableValues(terraformConfig, false)
+	}
+
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -62,9 +41,6 @@ func LoadVariablesFromFile(path, cwd string, applyTemplate bool, context ...inte
 	result, err := LoadVariablesFromSource(string(bytes), path, cwd, applyTemplate, context...)
 	if err != nil {
 		return nil, err
-	}
-	if len(result) == 1 && result["variable"] != nil {
-		return getDefaultVars(result)
 	}
 	return result, nil
 }
@@ -128,84 +104,30 @@ func LoadVariablesFromSource(content, fileName, cwd string, applyTemplate bool, 
 	return
 }
 
-// Returns the list of terraform files in a folder in alphabetical order (override files are always at the end)
-func getTerraformFiles(folder string) []string {
-	matches := map[string]int{}
-
-	// Resolve all patterns and add them to the matches map. Since the order is important (i.e. override files comes after non
-	// overridden files, we store the last pattern index in the map). f_override.tf will match both *.tf and *_override.tf, but
-	// will be associated with *_override.tf at the end, which is what is expected.
-	for i, pattern := range patterns {
-		files, err := filepath.Glob(filepath.Join(folder, pattern))
-		if err != nil {
-			panic(err)
+func getTerraformVariableValues(terraformConfig interface{}, includeNil bool) (map[string]interface{}, error) {
+	variables := []*configs.Variable{}
+	switch source := terraformConfig.(type) {
+	case *configs.Module:
+		for _, variable := range source.Variables {
+			variables = append(variables, variable)
 		}
-		for _, file := range files {
-			matches[file] = i
-		}
+	case *configs.File:
+		variables = source.Variables
 	}
-
-	// Then, we group files in two categories (regular and override) and we sort them alphabetically
-	var regularsFiles, overrideFiles []string
-	for file, index := range matches {
-		list := &regularsFiles
-		if index >= 2 {
-			// We group overrides files together
-			list = &overrideFiles
-		}
-		*list = append(*list, file)
-	}
-	sort.Strings(regularsFiles)
-	sort.Strings(overrideFiles)
-	return append(regularsFiles, overrideFiles...)
-}
-
-var patterns = []string{"*.tf", "*.tf.json", "override.tf", "override.tf.json", "*_override.tf", "*_override.tf.json"}
-
-func getDefaultVarsFromFile(filename string, unmarshal func([]byte, interface{}) error) (map[string]interface{}, error) {
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	content := make(map[string]interface{})
-	if err := unmarshal(bytes, &content); err != nil {
-		_, filename = filepath.Split(filename)
-		return nil, fmt.Errorf("%v %v", filename, err)
-	}
-
-	result, err := getDefaultVars(content)
-	if err != nil {
-		return result, fmt.Errorf("%v: %v", filename, err)
-	}
-
-	return result, nil
-}
-
-func getDefaultVars(content map[string]interface{}) (map[string]interface{}, error) {
-	var err error
-	result := make(map[string]interface{})
-
-	switch variables := content["variable"].(type) {
-	case map[string]interface{}:
-		addVariables(variables, result)
-	case []interface{}:
-		for _, value := range variables {
-			addVariables(value.(map[string]interface{}), result)
-		}
-	case nil:
-	default:
-		return nil, fmt.Errorf("Unknown variable type %[1]T: %[1]v", variables)
-	}
-
-	return result, err
-}
-
-func addVariables(source, target map[string]interface{}) {
-	for name, value := range source {
-		value := value.(map[string]interface{})
-		if value := value["default"]; value != nil {
-			target[name] = value
+	variablesMap := map[string]interface{}{}
+	for _, variable := range variables {
+		if includeNil || !variable.Default.IsNull() {
+			var value interface{}
+			marshallableVariable := ctyJson.SimpleJSONValue{Value: variable.Default}
+			marshalledVariable, err := marshallableVariable.MarshalJSON()
+			if err != nil {
+				return nil, err
+			}
+			if err := json.Unmarshal(marshalledVariable, &value); err != nil {
+				return nil, err
+			}
+			variablesMap[variable.Name] = value
 		}
 	}
+	return variablesMap, nil
 }
